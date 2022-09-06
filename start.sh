dec2d6c97d32bb615d144975cd0f5b112a69932c
Zack Ramjan <zack.hr@gmail.com>
2022-09-06 15:13:10 -0400

    added check for onboard nvme, format and mount if found

--- start.sh
+++ start.sh
@@ -5,6 +5,14 @@
 mkdir /efs
 mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport fs-09ddb74e0c26c1e69.efs.us-east-2.amazonaws.com:/ /efs
 
+#check if we are on a instance that has dedicated SSD
+if [ -b "/dev/nvme1n1" ]; 
+then
+    lsblk |grep nvme1n1 |grep -v home && mkfs.ext4 -E nodiscard /dev/nvme1n1
+    mkdir /home/ec2-user/SSD_tempspace
+    mount -o discard,noatime /dev/nvme1n1 /home/ec2-user/SSD_tempspace
+fi    
+
 #modules and module paths
 yum install -y environment-modules
 mkdir -p /cm/shared/modulefiles
